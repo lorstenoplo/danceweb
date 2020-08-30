f864cca8166169c2cac88f5c089d23bb0a0dc368
nishanth <gayatridipali@gmail.com>
2020-08-30 13:05:24 +0530

    Initing

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 80; 
+const bodyparser = require("body-parser")
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true,useUnifiedTopology: true});
+const port = process.env.PORT || 8080; 
+
+//defining mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
+
+const Contact = mongoose.model('Contact', contactSchema);
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
@@ -20,6 +34,18 @@ app.get('/', (req, res)=>{
 app.get('/contact', (req, res)=>{
     const params = {}
     res.status(200).render('contact.pug', params);
+})
+
+app.post('/contact', (req, res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.render("contact.pug")
+    }).catch(()=>{
+        res.statusCode(400).send("Your details couldn't be saved to database")
+    })
+    let messages = []
+    messages.push('your d has been saved')
+    // res.status(200).render('contact.pug');
 })
 
 // START THE SERVER
